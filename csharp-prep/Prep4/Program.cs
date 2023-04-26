ef05d7977308b23f6ed58b5fa09c181d6d588834
MrBenge <e.benge@gmx.at>
2023-04-27 00:19:22 +0200

    Wrote the program for the assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (enter 0 when you are done): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int Sum = 0;
+
+        foreach (int number in numbers)
+        {
+            Sum += number;
+        }
+
+        Console.WriteLine($"The sum of the list is {Sum}");
+
+        float Average = ((float)Sum) / numbers.Count;
+        Console.WriteLine($"The average of the list is {Average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        
+        Console.WriteLine($"The largest number is {max}");
     }
 }
