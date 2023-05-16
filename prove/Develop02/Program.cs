2604081558b7b20a5ab9073938a02da8b2f2e15a
MrBenge <e.benge@gmx.at>
2023-05-16 22:01:07 +0200

    Wrote the code for the program for last week

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+
+        Options option1 = new Options();
+        option1._introduction = "Please select one of the following choices";
+        option1._first = "Write";
+        option1._second = "Display";
+        option1._third = "Load";
+        option1._fourth = "Save";
+        option1._fifth = "Quit";
+
+        option1.Display();
+
+        string firstResponse = "-1";
+
+
+        Journal journal = new Journal();
+
+        do
+        {
+            Console.WriteLine("What would you like to do?");
+            string firstResponse = Console.ReadLine();
+
+            if (firstResponse == "1")
+            {
+                journal.Write();
+            }
+            else if (firstResponse == "2")
+            {
+                journal.DisplayIt();
+            }
+            else if (firstResponse == "3")
+            {
+                journal.Load();
+            }
+            else if (firstResponse == "4")
+            {
+                journal.Save();
+            }
+            while (firstResponse != "5");
     }
 }
---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Entry
+{
+    public string _dateTime;
+    public string _prompt;
+    public string _message;
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_dateTime} - {_prompt}");
+        Console.WriteLine($"{_message}");
+    }
+}
